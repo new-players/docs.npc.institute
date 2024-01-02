4c0f866e815e001de4e63c693c040476256f234f
Paul Gadi <paulgadi@gmail.com>
2024-01-02 15:40:06 -0500

    update boilerplate code

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+const logo = 'logo.jpg'
+
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: 
+    <span>
+      <img src={logo} alt="New Players Cooperative" width="50px" height="50px" />
+    </span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/new-players',
   },
+  /*
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  */
+  docsRepositoryBase: 'https://github.com/new-players/docs.npc.institute',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'MIT 2024 © New Players Cooperative',
   },
 }
 
