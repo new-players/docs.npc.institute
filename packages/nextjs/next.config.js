2fada808fdbdf08b6d7a1dc1c6ab887925103222
Paul Gadi <paulgadi@gmail.com>
2024-02-07 19:43:49 -0500

    add config settings from scaffold-eth

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,6 +1,17 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },  
 })
 
 module.exports = withNextra()
