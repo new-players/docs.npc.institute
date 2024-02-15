ef1b88966411fb8f7e9fc2320c9a4ca82faa830c
Paul Gadi <paulgadi@gmail.com>
2024-02-14 22:20:17 -0500

    update logo

--- packages/nextjs/theme.config.tsx
+++ packages/nextjs/theme.config.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { RainbowKitCustomConnectButton } from './components/RainbowKitCustomConnectButton'
+import localFont from 'next/font/local'
+import styles from './logo.module.css'
 
-const logo = 'logo.jpg'
+const logo = 'logo.svg'
 const opg = 'opg.png'
+const myFont = localFont({src: './fonts/RifficFree-Bold.ttf'})
 
 const config: DocsThemeConfig = {
   logo: 
-    <span>
-      <img src={logo} alt="npc.institute" width="110px" />
-    </span>,
+  (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ flex: 1 }}>
+        <img src={logo} alt="npc.institute" width="40px" />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 5 }}>
+        <span className={`${myFont.className} ${styles.logotext}`} >
+          NPC
+        </span>
+        <span className={`${myFont.className} ${styles.logotext2}`} >
+          INSTITUTE
+        </span>
+      </div>
+    </div>
+  ),
   project: {
     link: 'https://github.com/new-players',
   },
