ec2320715a6ca685ee8f490cbfcb0421562d59f4
Paul Gadi <paulgadi@gmail.com>
2024-04-09 18:09:36 -0400

    disable slack and github buttons, add discord

--- packages/nextjs/theme.config.tsx
+++ packages/nextjs/theme.config.tsx
@@ -23,24 +23,26 @@ const config: DocsThemeConfig = {
           INSTITUTE
         </span>
       </div>
+      <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 10, justifyContent: "center", marginTop: 10 }}>
+        <span className={`${myFont.className} ${styles.logotext2}`} >
+          by
+        </span>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 10, justifyContent: "center"}}>
+
+        <span>
+            <img src={opg} alt="npc.institute" width="80px" />
+        </span>          
+      </div>
+
+
+
     </div>
   ),
-  project: {
-    link: 'https://github.com/new-players',
-  },
   chat: {
-    link: 'https://join.slack.com/t/newplayerscooperative/shared_invite/zt-2dcakmn2r-mCT19iMCin3w0R~M5qGv6Q',
-    icon: (
-      <svg width="24" height="24" viewBox="0 0 60 60">
-        <g fill="currentColor">
-          <path d="M22,12 a6,6 0 1 1 6,-6 v6z M22,16 a6,6 0 0 1 0,12 h-16 a6,6 0 1 1 0,-12"/>
-          <path d="M48,22 a6,6 0 1 1 6,6 h-6z M32,6 a6,6 0 1 1 12,0v16a6,6 0 0 1 -12,0z"/>
-          <path d="M38,48 a6,6 0 1 1 -6,6 v-6z M54,32 a6,6 0 0 1 0,12 h-16 a6,6 0 1 1 0,-12"/>
-          <path d="M12,38 a6,6 0 1 1 -6,-6 h6z M16,38 a6,6 0 1 1 12,0v16a6,6 0 0 1 -12,0z"/>
-        </g>
-     </svg>
-    )
+    link: 'https://discord.gg/jWGttTYK'
   },
+
   docsRepositoryBase: 'https://github.com/new-players/docs.npc.institute',
   sidebar: {
     titleComponent({ title, type }) {
@@ -71,9 +73,31 @@ const config: DocsThemeConfig = {
   primarySaturation: 100,
   navbar: {
     extraContent: (
-      <RainbowKitCustomConnectButton />
+      <>
+      {/* <RainbowKitCustomConnectButton /> */}
+      </>
     )
   }  
 }
 
 export default config
+
+
+/*
+  project: {
+    link: 'https://github.com/new-players',
+  },
+  chat: {
+    link: 'https://join.slack.com/t/newplayerscooperative/shared_invite/zt-2dcakmn2r-mCT19iMCin3w0R~M5qGv6Q',
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 60 60">
+        <g fill="currentColor">
+          <path d="M22,12 a6,6 0 1 1 6,-6 v6z M22,16 a6,6 0 0 1 0,12 h-16 a6,6 0 1 1 0,-12"/>
+          <path d="M48,22 a6,6 0 1 1 6,6 h-6z M32,6 a6,6 0 1 1 12,0v16a6,6 0 0 1 -12,0z"/>
+          <path d="M38,48 a6,6 0 1 1 -6,6 v-6z M54,32 a6,6 0 0 1 0,12 h-16 a6,6 0 1 1 0,-12"/>
+          <path d="M12,38 a6,6 0 1 1 -6,-6 h6z M16,38 a6,6 0 1 1 12,0v16a6,6 0 0 1 -12,0z"/>
+        </g>
+     </svg>
+    )
+  },  
+*/
